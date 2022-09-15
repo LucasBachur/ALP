0795424213f4bd30a616a15715ef88e2a88e6a85
blasbarbagelata <blasbarbagelata@gmail.com>
2022-09-15 17:06:07 -0300

    hechos los comandos del eval1

--- src/Eval1.hs
+++ src/Eval1.hs
@@ -7,6 +7,8 @@ where
 import           AST
 import qualified Data.Map.Strict               as M
 import           Data.Strict.Tuple
+import Text.ParserCombinators.ReadPrec (step)
+import GHC.Base (seq)
 
 -- Estados
 type State = M.Map Variable Int
@@ -14,17 +16,17 @@ type State = M.Map Variable Int
 -- Estado nulo
 -- Completar la definición
 initState :: State
-initState = undefined
+initState = M.empty
 
 -- Busca el valor de una variable en un estado
 -- Completar la definición
 lookfor :: Variable -> State -> Int
-lookfor v s = undefined
+lookfor v s = s M.! v
 
 -- Cambia el valor de una variable en un estado
 -- Completar la definición
 update :: Variable -> Int -> State -> State
-update = undefined
+update = M.insert
 
 -- Evalua un programa en el estado nulo
 eval :: Comm -> State
@@ -39,7 +41,15 @@ stepCommStar c    s = Data.Strict.Tuple.uncurry stepCommStar $ stepComm c s
 -- Evalua un paso de un comando en un estado dado
 -- Completar la definición
 stepComm :: Comm -> State -> Pair Comm State
-stepComm = undefined
+stepComm Skip s= (Skip :!: s)
+stepComm (Let var exp) s = let (n :!: s') = evalExp s
+                           in (Skip :!: update var n s')
+stepComm (Seq Skip c) s = (c :!: s)
+stepComm (Seq com1 com2) s = let (com :!: s') = stepComm com1 s
+                             in (Seq com com2 :!: s')
+stepComm (IfThenElse bexp c1 c2) s = let (b :!: s') = evalExp bexp s
+                                     in if b then (c1 :!: s') else (c2 :!: s')
+stepComm loop@(While bexp c) s = ((Seq (IfThenElse bexp Skip loop) c ) :!: s)
 
 -- Evalua una expresion
 -- Completar la definición
