38e16728131cd2235f5691ee05b901308c956719
blasbarbagelata <blasbarbagelata@gmail.com>
2022-09-17 15:53:17 -0300

    agregue evalExp en eval1, falta Econd y ver while

--- src/Eval1.hs
+++ src/Eval1.hs
@@ -53,5 +53,43 @@ stepComm loop@(While bexp c) s = ((Seq (IfThenElse bexp Skip loop) c ) :!: s)
 
 -- Evalua una expresion
 -- Completar la definición
-evalExp :: Exp a -> State -> a 
-evalExp = undefined
+evalExp :: Exp a -> State -> Pair a State
+evalExp (Const n) s = (n :!: s)
+evalExp (Var v) s = (lookfor v s :!: s)
+evalExp (UMinus exp) s = let (n :!: s') = evalExp exp s in (-n :!: s')
+evalExp (Plus exp1 exp2) s = let (n1 :!: s') = evalExp exp1 s
+                                 (n2 :!: s'') = evalExp exp2 s'
+                             in (n1 + n2 :!: s'')
+evalExp (Minus exp1 exp2) s = let (n1 :!: s') = evalExp exp1 s
+                                  (n2 :!: s'') = evalExp exp2 s'
+                              in (n1 - n2 :!: s'')
+evalExp (Times exp1 exp2) s = let (n1 :!: s') = evalExp exp1 s
+                                  (n2 :!: s'') = evalExp exp2 s'
+                              in (n1 * n2 :!: s'')
+evalExp (Div exp1 exp2) s = let (n1 :!: s') = evalExp exp1 s
+                                (n2 :!: s'') = evalExp exp2 s'
+                            in (div n1 n2 :!: s'')
+evalExp (Econd expbool exp1 exp2) s = 
+
+evalExp BTrue s = (True :!: s)
+evalExp BFalse s = (False :!: s)
+evalExp (Lt exp1 exp2) s = let (b1 :!: s') = evalExp exp1 s
+                               (b2 :!: s'') = evalExp exp2 s'
+                           in (b1 < b2 :!: s'')
+evalExp (Gt exp1 exp2) s = let (b1 :!: s') = evalExp exp1 s
+                               (b2 :!: s'') = evalExp exp2 s'
+                           in (b1 > b2 :!: s'')
+evalExp (And exp1 exp2) s = let (b1 :!: s') = evalExp exp1 s
+                                (b2 :!: s'') = evalExp exp2 s'
+                           in ((b1 && b2) :!: s'')
+evalExp (Or exp1 exp2) s = let (b1 :!: s') = evalExp exp1 s
+                               (b2 :!: s'') = evalExp exp2 s'
+                           in ((b1 || b2) :!: s'')
+evalExp (Not exp) s = let (b :!: s') = evalExp exp s
+                      in (not b :!: s')
+evalExp (Eq exp1 exp2) s = let (b1 :!: s') = evalExp exp1 s
+                               (b2 :!: s'') = evalExp exp2 s'
+                           in (b1 == b2 :!: s'') 
+evalExp (NEq exp1 exp2) s = let (b1 :!: s') = evalExp exp1 s
+                                (b2 :!: s'') = evalExp exp2 s'
+                            in (b1 /= b2 :!: s'')
